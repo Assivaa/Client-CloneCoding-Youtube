70093216e2ffa61e7d74591dcc666d9d4378a9f1
asiva syhrm <assiva.syahrani9e@gmail.com>
2022-12-13 16:36:37 +0700

    first

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,111 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import SearchIcon from "@mui/icons-material/Search";
+import VideoCallOutlinedIcon from "@mui/icons-material/VideoCallOutlined";
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { logout } from "../redux/userSlice";
+import Upload from "./Upload";
+
+const Container = styled.div`
+	position: sticky;
+	top: 0;
+	background-color: ${({ theme }) => theme.bgLighter};
+	height: 56px;
+`;
+const Wrapper = styled.div`
+	display: flex;
+	align-items: center;
+	justify-content: flex-end;
+	padding: 0px 20px;
+	height: 100%;
+	position: relative;
+`;
+const Search = styled.div`
+	position: absolute;
+	width: 50%;
+	left: 0px;
+	right: 0px;
+	margin: auto;
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	padding: 5px;
+	border: 1px solid #ccc;
+`;
+const Input = styled.input`
+	border: none;
+	background-color: transparent;
+`;
+const Button = styled.button`
+	padding: 5px 15px;
+	background-color: transparent;
+	border: 1px solid #3ea6ff;
+	color: #3ea6ff;
+	border-radius: 3px;
+	font-weight: 500;
+	cursor: pointer;
+	display: flex;
+	align-items: center;
+	gap: 5px;
+`;
+const User = styled.div`
+	display: flex;
+	align-items: center;
+	gap: 10px;
+	font-weight: 500;
+	color: ${({ theme }) => theme.text};
+`;
+const Avatar = styled.img`
+	width: 32px;
+	height: 32px;
+	border-radius: 50%;
+	background-color: #999;
+`;
+
+const Navbar = () => {
+	const { currentUser } = useSelector((state) => state.user);
+	const dispatch = useDispatch();
+	const handleLogout = async (e) => {
+		e.preventDefault();
+		dispatch(logout());
+		navigate("/");
+	};
+
+	const [open, setOpen] = useState(false);
+	return (
+		<div>
+			<Container>
+				<Wrapper>
+					<Search>
+						<Input placeholder="Search" />
+						<SearchIcon />
+					</Search>
+					{currentUser ? (
+						<>
+							<User>
+								<VideoCallOutlinedIcon onClick={() => setOpen(true)} />
+								<Avatar src={currentUser.profileImg} />
+								{currentUser.username}
+							</User>
+							<p style={{ color: "#fff", marginLeft: "20px" }} onClick={handleLogout}>
+								Logout
+							</p>
+						</>
+					) : (
+						<Link to="signin" style={{ textDecoration: "none" }}>
+							<Button>
+								<AccountCircleIcon />
+								SIGN IN
+							</Button>
+						</Link>
+					)}
+				</Wrapper>
+			</Container>
+			{open && <Upload setOpen={setOpen} />}
+		</div>
+	);
+};
+
+export default Navbar;
---
+++ src/pages/SignIn.jsx
@@ -0,0 +1,99 @@
+import axios from "axios";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import styled from "styled-components";
+import { loginFailure, loginStart, loginSuccess } from "../redux/userSlice";
+import { useNavigate } from "react-router-dom";
+
+const Container = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	height: calc (100vh - 56px);
+	color: ${({ theme }) => theme.text};
+`;
+const Wrapper = styled.div`
+	display: flex;
+	align-items: center;
+	flex-direction: column;
+	background-color: ${({ theme }) => theme.bgLighter};
+	border: 1px solid ${({ theme }) => theme.soft};
+	padding: 20px 50px;
+	gap: 10px;
+`;
+//style cssnya
+const Title = styled.h1`
+	font-size: 24px;
+`;
+const SubTitle = styled.h2`
+	font-size: 20px;
+	font-weight: 300;
+`;
+const Input = styled.input`
+	border: 1 px solid ${({ theme }) => theme.soft};
+	border-radius: 3px;
+	padding: 10px;
+	background-color: white;
+	width: 100%;
+`;
+const Button = styled.button`
+	border-radius: 3px;
+	border: none;
+	padding: 10px 20px;
+	font-weight: 500;
+	cursor: pointer;
+	background-color: ${({ theme }) => theme.soft};
+	background-color: ${({ theme }) => theme.textSoft};
+`;
+const More = styled.div`
+	display: flex;
+	margin-top: 10px;
+	font-size: 12px;
+	color: ${({ theme }) => theme.textSoft};
+`;
+const Links = styled.div`
+	margin-left: 50px;
+`;
+const Link = styled.span`
+	margin-left: 30px;
+`;
+
+//edit sign in
+const SignIn = () => {
+	const [username, setName] = useState("");
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const dispatch = useDispatch();
+	const navigate = useNavigate();
+
+	const handleLogin = async (e) => {
+		e.preventDefault();
+		dispatch(loginStart());
+		try {
+			const { data } = await axios.post(`auth/signin`, { username, password });
+			dispatch(loginSuccess(data));
+			navigate("/");
+		} catch (err) {
+			dispatch(loginFailure());
+		}
+	};
+	return (
+		<Container>
+			<Wrapper>
+				<Title>Sign in</Title>
+				{/* <SubTitle>to continue to LamaTube</SubTitle> */}
+				<Input placeholder="username" onChange={(e) => setName(e.target.value)} />
+				<Input type="password" placeholder="password" onChange={(e) => setPassword(e.target.value)} />
+				<Button onClick={handleLogin}>Sign in</Button>
+				{/* <SubTitle>or</SubTitle>
+				<Input placeholder="username" onChange={(e) => setName(e.target.value)} />
+				<Input placeholder="email" onChange={(e) => setEmail(e.target.value)} />
+				<Input type="password" placeholder="password" onChange={(e) => setPassword(e.target.value)} />
+				<Button>Sign up</Button> */}
+			</Wrapper>
+		</Container>
+	);
+};
+
+export default SignIn;
